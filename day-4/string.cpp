b9b09b90f0d2bf72f70de315a1091f3dce3591bc
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-12-23 10:12:31 +0600

    string explored in cpp

---
+++ day-4/string.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string studentName = "Kuddus Ali";
+    string universityName = "Mokoddos Ali university";
+    cout<<endl<<"Student Name : "<<studentName<<". University Name : "<<universityName<<endl;
+    return 0;
+}
