dc8f87d4e3cc38068454bd25cb1a23f2cb75203b
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-12-29 11:42:38 +0600

    problem using pairs and vectors has bee solved in cpp

--- day-8/vectorInCpp.cpp
+++ day-8/vectorInCpp.cpp
@@ -1,14 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void printVec(vector<int> v){
+void printVec(vector<int> &v){
     int size = v.size();
-    cout<<"size: "<<size<<endl;
+    // cout<<"size: "<<size<<endl;
     for (int i = 0; i < size; i++)
     {
         cout<<v[i]<<" ";
     }
-    
+    v.push_back(222);
+    cout<<endl;
 }
 
 int main(){
@@ -17,8 +18,15 @@ int main(){
     cin>>n;
     for (int i = 1; i <= n; i++)
     {
-        myNumb.push_back(i);
+        int x;
+        cin>>x;
+        // printVec(myNumb);
+        myNumb.push_back(x);
     }
     printVec(myNumb);
+
+    vector<int> v2 = myNumb; // O(n)
+    printVec(v2);
+
     return 0;
 }
