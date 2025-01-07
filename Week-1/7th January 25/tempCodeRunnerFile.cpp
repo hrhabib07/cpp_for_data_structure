52e75206ba6b3494a26eb9f184e48373be0aea08
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2025-01-07 07:24:52 +0600

    problem 2 has been solved

--- Week-1/7th January 25/tempCodeRunnerFile.cpp
+++ Week-1/7th January 25/tempCodeRunnerFile.cpp
@@ -1,1 +1,11 @@
-97
+  // for (int i = 0; i < s.size(); i++)
+    // {
+    //     if(i==0){
+    //     char ch = toupper(s[i]);
+    //     cout<<ch;
+    //     s[i]= toupper(s[i]);
+    //     }else{
+    //         cout<<s[i];
+    //     }
+    // }
+    
