891b330505084e2ab6c2c1a48c5eb9512a30b183
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2025-01-20 17:31:37 +0600

    new problem sovled

--- January 25/week-3/day-3/A_Fibonacciness.cpp
+++ January 25/week-3/day-3/A_Fibonacciness.cpp
@@ -7,24 +7,25 @@ int main(){
     for (int i = 0; i < n; i++)
     {
         vector<int>inp;
+        int count=0;
         int a1,a2,a4,a5;
         cin>>a1>>a2>>a4>>a5;
-        if(a1+a2==a5-a4){
-            v.push_back(3);
-        } else if (a1+a2!=a5-a4){
-            int diff=a5-a4;
-            int a3=diff;
-            if(a3+a2==a4){
-                v.push_back(2);
-            } else if(a1+a2==a4-(a1+a2)){
-                v.push_back(2);
-            } else if(a1+a2==a4-(a2)){
-                v.push_back(2);
-            } else{
-                v.push_back(1);
-            }
+        int a3=a1+a2;
+        if(a1+a2==a3){
+            count++;
         }
-
+        if(a3+a2==a4){
+            count++;
+        } if(a3+a4==a5){
+            count++;
+        }
+        if(count==1){
+            int na3=a5-a4;
+            if(na3+a2==a4){
+                count++;
+            }
+        };
+        v.push_back(count);
     };
     for (int i = 0; i < v.size(); i++)
     {
--- January 25/week-3/day-3/C_Game_of_Mathletes.cpp
+++ January 25/week-3/day-3/C_Game_of_Mathletes.cpp
@@ -1,38 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     int t;
-    cin>>t;
-    vector<int>v;
-    for (int k = 0; k < t; k++)
-    {
-        int n,m;
-        cin>>n>>m;
-        vector<int>inp;
-        for (int i = 0; i < n; i++)
-        {
-            int x;
-            cin>>x;
-            inp.push_back(x);
-        };
+    cin >> t;
+    vector<int> v;
+    for (int k = 0; k < t; k++) {
+        int n, m;
+        cin >> n >> m;
+        vector<int> inp(n);
+        for (int i = 0; i < n; i++) {
+            cin >> inp[i];
+        }
+        unordered_map<int, int> freq;
         int score = 0;
-        for (int i = 0; i < n; i++)
-        {
-            int current =  inp[i];
-            for (int j = i+1; j < n; j++)
-            {
-                if(current+inp[i]==m){
-                    score++;  
-                }
+        for (int x : inp) {
+            int complement = m - x; 
+            if (freq[complement] > 0) {
+                score++;
+                freq[complement]--; 
+            } else {
+                freq[x]++;
             }
         }
         v.push_back(score);
     }
-    for (int i = 0; i < t; i++)
+    for (int i = 0; i < v.size(); i++)
     {
         cout<<v[i]<<endl;
     }
-    
     return 0;
 }
