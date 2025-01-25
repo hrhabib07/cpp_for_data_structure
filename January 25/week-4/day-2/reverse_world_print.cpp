c4e237ae6706c87f5d2150cf870a6f21018fb76b
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2025-01-25 17:34:18 +0600

    football problem solved

--- January 25/week-4/day-2/reverse_world_print.cpp
+++ January 25/week-4/day-2/reverse_world_print.cpp
@@ -1,14 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
+void print(stringstream& ss){
+    string word;
+    if(ss>>word){
+        print(ss);
+        cout<<word<<endl;
+    }
+}
+
 int main(){
     string s;
     getline(cin,s); // get line takes input with space; we use it for taking sentence
     stringstream ss(s);
-    string word;
-    while (ss>>word)
-    {
-        cout<<word<<endl;
-    }
+    print(ss);
+    // string word;
+    // while (ss>>word)
+    // {
+    //     cout<<word<<endl;
+    // }
     
     return 0;
 }
