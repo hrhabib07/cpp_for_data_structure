f414b8185bb114a7611101dae7cb93b30d9e0d08
hrhabib07 <mdhabibur.hr7@gmail.com>
2025-02-23 08:59:10 +0600

    new problem sovled

---
+++ C_Vasilije_in_Cacak.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long int n, k, x;
+        cin >> n >> k >> x;
+        long long int sum = (n * (n + 1)) / 2;
+        cout << sum << endl;
+        // if (k < x && sum >= x)
+        // {
+
+        //     cout << "YES\n";
+        // }
+        // else
+        // {
+        //     cout << "NO\n";
+        // }
+    }
+
+    return 0;
+}
