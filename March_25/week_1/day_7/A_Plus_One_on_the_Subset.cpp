bcb55d07305d0404194b13734b492bacd7708bae
hrhabib07 <mdhabibur.hr7@gmail.com>
2025-03-07 15:29:23 +0600

    new problem solved

---
+++ March_25/week_1/day_7/A_Plus_One_on_the_Subset.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    };
+    sort(v.begin(), v.end());
+    cout << v[n - 1] - v[0] << '\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
