17b9885fa609fc3b791ef92713229998cfbbc3f8
hrhabib07 <mdhabibur.hr7@gmail.com>
2025-03-08 23:35:30 +0600

    new problem sovled

---
+++ March_25/week_2/day_1/A_Marathon.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    int count = 0;
+    if (b > a)
+        count++;
+    if (c > a)
+        count++;
+    if (d > a)
+        count++;
+    cout << count << '\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
