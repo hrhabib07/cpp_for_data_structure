8bf0033b8b351ecee28225e7903b80da68c9866c
hrhabib07 <mdhabibur.hr7@gmail.com>
2025-05-27 17:36:23 +0600

    new problem solved

---
+++ 2025/5.May_25/Last_week/Tuesday/A_Fashionable_Array.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin>>t;
+    while (t--)
+    {
+        int n; cin>>n;
+        vector<int>v(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin>>v[i];
+        }
+        sort(v.begin(),v.end());
+        for (int i = 0; i < n; i++)
+        {
+            v[i]=v[i]%2;
+        }
+        if(v[0]==v[n-1] && n>1) cout<<0<<endl;
+        else if(n==1){
+            // if(v[0]%2) cout<<1<<endl;
+            cout<<0<<endl;
+        }
+        else{
+            int cl=1,cr=1;
+            for (int i = 1; i < n; i++)
+            {
+                if(v[i]==v[i-1]) cl++;
+                else break;
+            }
+            for (int i = n-1; i >= 0; i--)
+            {
+                if(v[i]==v[i-1]) cr++;
+                else break;
+            }
+            if(cr>=cl) cout<<cl<<endl;
+            else cout<<cr<<endl;
+            
+        } 
+
+    }
+    
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long int a = INT64_MIN;
+    cout<<a<<" "<<endl;
+    return 0;
+}
