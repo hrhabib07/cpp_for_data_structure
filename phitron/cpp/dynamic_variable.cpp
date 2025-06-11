58d613d30f1e7b77a1d2a083c8f62949e734e9ee
hrhabib07 <mdhabibur.hr7@gmail.com>
2025-06-11 16:56:59 +0600

    new topic explored

---
+++ phitron/cpp/dynamic_variable.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x = 10; // static variable
+    int *p = new int;  // dynamic variable
+    *p = 100; 
+    cout<<*p<<endl;
+    return 0;
+}
