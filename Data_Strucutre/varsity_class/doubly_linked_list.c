d7d0caef248eeb07d56f0cf3c579e82b40c43ba2
hrhabib07 <mdhabibur.hr7@gmail.com>
2025-07-11 16:22:31 +0600

    new code added

---
+++ Data_Strucutre/varsity_class/doubly_linked_list.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *prev;
+    struct Node *next;
+};
+
+void display(struct Node *temp)
+{
+    while (temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    struct Node *head, *temp, *newnode;
+    int choice = 1;
+    head = 0;
+
+    while (choice)
+    {
+        newnode = (struct Node *)malloc(sizeof(struct Node));
+        printf("Current List : \n");
+        display(head);
+        printf("\n");
+
+        printf("Enter Data : ");
+        scanf("%d", &newnode->data);
+        newnode->prev = 0;
+        newnode->next = 0;
+        if (head == 0)
+        {
+            head = newnode;
+            temp = newnode;
+        }
+        else
+        {
+            newnode->prev = temp;
+            temp->next = newnode;
+            temp = newnode;
+        }
+        printf("Do you want to Continueue? If yes press 1 otherwise press 0.");
+        scanf("%d", &choice);
+    }
+
+    return 0;
+}#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *prev;
+    struct Node *next;
+};
+
+void display(struct Node *temp)
+{
+    while (temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    struct Node *head, *temp, *newnode;
+    int choice = 1;
+    head = 0;
+
+    while (choice)
+    {
+        newnode = (struct Node *)malloc(sizeof(struct Node));
+        printf("Current List : \n");
+        display(head);
+        printf("\n");
+
+        printf("Enter Data : ");
+        scanf("%d", &newnode->data);
+        newnode->prev = 0;
+        newnode->next = 0;
+        if (head == 0)
+        {
+            head = newnode;
+            temp = newnode;
+        }
+        else
+        {
+            newnode->prev = temp;
+            temp->next = newnode;
+            temp = newnode;
+        }
+        printf("Do you want to Continueue? If yes press 1 otherwise press 0.");
+        scanf("%d", &choice);
+    }
+
+    return 0;
+}
