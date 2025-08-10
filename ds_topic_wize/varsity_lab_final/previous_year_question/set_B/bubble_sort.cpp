9f3bcfd22a2c463f90623cb14dd901cc501c62a2
hrhabib07 <mdhabibur.hr7@gmail.com>
2025-08-10 13:39:53 +0600

    new code added

--- ds_topic_wize/varsity_lab_final/previous_year_question/set_B/bubble_sort.cpp
+++ ds_topic_wize/varsity_lab_final/previous_year_question/set_B/bubble_sort.cpp
@@ -15,9 +15,9 @@ int main() {
 
     for (int i = 0; i < n-1; i++)
     {
-        for (int j = i+1; j < n; j++)
+        for (int j = 0; j < n-i-1; j++)
         {
-            if(v[i]<v[j]) swap(v[i],v[j]);
+            if(v[j+1]<v[j]) swap(v[j+1],v[j]);
         }
     }
 
