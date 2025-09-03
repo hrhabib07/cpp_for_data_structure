b405ea69d584015da276809d10892bb7b1de86b5
hrhabib07 <mdhabibur.hr7@gmail.com>
2025-09-03 15:28:46 +0600

    new code added

---
+++ ds_topic_wize/graph/bfs_on_2d_grid.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+char grid[105][105];
+bool vis[105][105];
+vector<pair<int,int>>d={{0,-1},{0,1},{-1,0},{1,0}};
+int row,col;
+bool valid(int i,int j){
+    if(i<0 || i>=row || j<0 || j>=col)
+        return false;
+    return true;
+}
+
+void bfs(int si,int sj){
+    queue<pair<int,int>>q;
+    q.push({si,sj});
+    vis[si][sj]= true;
+
+    while(!q.empty()){
+        pair<int,int>par = q.front();
+        q.pop();
+        int par_i = par.first;
+        int par_j = par.second;
+        cout<<par_i<<" "<<par_j<<endl; 
+        
+        for (int i = 0; i < 4; i++)
+        {
+            int ci = par_i+d[i].first;
+            int cj = par_j+d[i].second;
+            if(valid(ci,cj) && !vis[ci][cj]){
+                q.push({ci,cj});
+                vis[ci][cj]=true;
+            }
+        }
+    }
+}
+int main() {
+    cin>>row>>col;
+    for (int i = 0; i < row; i++)
+    {
+        for(int j = 0; j<col;j++){
+            cin>>grid[i][j];
+        }
+    }
+    memset(vis,false,sizeof(vis));
+    int si,sj;
+    cin>>si>>sj;
+    bfs(si,sj);
+
+    return 0;
+}
